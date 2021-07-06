cf41eccc448b93add402fcb2eccdf69177b16962
Denis_Cera <dene@move.ai>
2021-07-06 12:51:32 +0300

    intial

---
+++ Source/gRPCLiveLinkProject/gRPCLiveLinkProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "gRPCLiveLinkProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gRPCLiveLinkProject, "gRPCLiveLinkProject" );
---
+++ Source/gRPCLiveLinkProject/gRPCLiveLinkProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gRPCLiveLinkProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRPCLIVELINKPROJECT_API AgRPCLiveLinkProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
