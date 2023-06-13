e6634e754caed16c75b77d468dcad16a260dc98f
Denis_Cera <dene@move.ai>
2023-06-13 13:38:02 +0300

    Option to print debug info in game

--- Plugins/ProtobufLiveLink/Source/ProtobufLiveLink/Private/LogUtility.cpp
+++ Plugins/ProtobufLiveLink/Source/ProtobufLiveLink/Private/LogUtility.cpp
@@ -2,7 +2,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 
-void LogCurrentTimeWithMilliseconds(const FString& LogMessage)
+void LogCurrentTimeWithMilliseconds(const FString& LogMessage, bool bPrintToGameWindow)
 {
     FDateTime CurrentTime = FDateTime::Now();
     int32 Milliseconds = CurrentTime.GetMillisecond();
@@ -12,4 +12,18 @@ void LogCurrentTimeWithMilliseconds(const FString& LogMessage)
     FString FullLogMessage = FString::Printf(TEXT("%s: %s.%03d"), *LogMessage, *TimeString, Milliseconds);
 
     UE_LOG(LogTemp, Warning, TEXT("%s"), *FullLogMessage);
+
+    // Print the log message on the game window if enabled
+    if (bPrintToGameWindow && GEngine)
+    {
+        FColor Color = FColor::Yellow; // Choose the color for the debug message
+        float Duration = 5.0f; // Duration in seconds for how long the message will be visible
+
+        // Use the following line if you want to print the message at the bottom of the screen
+        GEngine->AddOnScreenDebugMessage(-1, Duration, Color, FullLogMessage);
+
+        // Use the following line if you want to print the message at a specific location on the screen
+        // FVector2D ScreenPosition(100, 100); // Specify the X and Y screen coordinates
+        // GEngine->AddOnScreenDebugMessage(-1, Duration, Color, FullLogMessage, false, ScreenPosition);
+    }
 }
